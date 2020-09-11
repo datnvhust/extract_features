efbb405334539cb62ae79ced1629e7ea55de5fee
datnv <dat.nv160952@sis.hust.edu.vn>
2020-09-11 11:42:33 +0700

    Change

--- buglocalizer/similar_report.py
+++ buglocalizer/similar_report.py
@@ -1,17 +1,95 @@
 import pickle
 import json
 from collections import OrderedDict
-
+from math import log
+from sklearn.metrics.pairwise import cosine_similarity
+from numpy.linalg import norm
 from datasets import DATASET
 import datetime
 import os.path
 import numpy as np
 
+class TFIDFVectorizer():
+    def __init__(self, k=1.5, b=0.75):
+        self.k = k
+        self.b = b
+
+    def tf(self,word,doc, doc_list):
+        all_num=sum([doc[key] for key in doc])
+        avg = 0
+        for vb in doc_list:
+            avg += sum([vb[key] for key in vb])
+        avg = avg/len(doc_list)
+        return (doc[word]*(self.k + 1))/(doc[word] + self.k*(1 - self.b + self.b*all_num/avg))
+
+    def idf(self, word,doc_list):
+        all_num=len(doc_list)
+        word_count=0
+        for doc in doc_list:
+            if word in doc:
+                word_count+=1
+        return log((all_num+1)/(word_count+0.5))
+
+    def tfidf(self, word,doc,doc_list):
+        score = self.tf(word,doc, doc_list)*self.idf(word,doc_list)
+        return score
+    def compute_tfidf_summary(self, bug_reports):
+        vocab = []
+        docs = []
+        for report in bug_reports.values():
+            doc = {}
+            for word in report.summary['unstemmed']:
+                if word not in vocab:
+                    vocab.append(word)
+                if word in doc.keys():
+                    doc[word] += 1
+                else:
+                    doc[word] = 1
+
+            for word in report.description['unstemmed']:
+                if word not in vocab:
+                    vocab.append(word)
+                if word in doc.keys():
+                    doc[word] += 1
+                else:
+                    doc[word] = 1
+            docs.append(doc)
+        x_tfidf = []
+        for doc in docs:
+            for word in doc:
+                doc[word] = self.tfidf(word,doc,docs)
+            row = []
+            for f in vocab:
+                if f in doc.keys():
+                    row.append(doc[word])
+                else:
+                    row.append(0)
+            x_tfidf.append(row)
+        # should save sparse matrix
+        return x_tfidf
+    def compute_similarity(self, x_tfidf, y_tfidf):
+        """
+        compute similarity
+        """
+        n = len(x_tfidf)
+        size = len(x_tfidf[n-1])
+        x_tfidf = np.asarray(x_tfidf)
+        x_similarity = np.empty((n, n), dtype=object) 
+        for i in range(n):
+            for j in range(n):     
+                x_similarity[i][j] = cosine_similarity(x_tfidf[i].reshape(1,size), y_tfidf[j].reshape(1,size))[0][0]
+        return x_similarity
 
 def similar_report_scores(src_files, bug_reports):
     # duyệt từng bug
+    tf = TFIDFVectorizer()
+    x_tfidf = tf.compute_tfidf_summary(bug_reports)
+    n = len(x_tfidf)
+    size = len(x_tfidf[n-1])
+    x_tfidf = np.asarray(x_tfidf)
     similarity_report_scores = []
-    for report in bug_reports.values():
+    for r1, report in enumerate(bug_reports.values()):
+        print(r1)
         scores = []
         input_bug_report = int(report.opendate)
 
@@ -36,13 +114,13 @@ def similar_report_scores(src_files, bug_reports):
                         f = f.split('\\')[-1]
                         exact_file_name = src.exact_file_name.split(' ')[-1]
                         if(exact_file_name == f):
-                            print(relevant_commit.summary['unstemmed'])
-                            print(report.summary['unstemmed'])
+                            # print(relevant_commit.summary['unstemmed'])
+                            # print(report.summary['unstemmed'])
                             # tc = (input_bug_report - datetime.datetime.strptime(
                             #     relevant_commit.fixdate, '%Y-%m-%d %H:%M:%S').timestamp()) / 60/60/24
                             # tc = (input_bug_report - int(relevant_commit.fixdate)) / 60 / 60 / 24
                             
-                            # simi_score += 1 / ( 1 + np.exp(tc / k))
+                            simi_score += cosine_similarity(x_tfidf[r1].reshape(1,size), x_tfidf[i].reshape(1,size))[0][0]
                    
                 scores.append(simi_score)
             else:
